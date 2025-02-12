182679940081070a0147c006b314a23054a1ab96
Wellington Garcia <garciawell@gmail.com>
2025-02-12 00:58:17 -0300

    feat: validation fields

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -4,33 +4,55 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/garciawell/pos-go-rate-limiter/configs"
 )
 
-var limiterData = make(map[string]int)
+type LimiterInfo struct {
+	Count          int
+	BlockTimestamp time.Time
+}
+
+var limiterData = make(map[string]LimiterInfo)
 
 func RateLimitMiddleware(next http.Handler, ENVS *configs.Conf) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip := r.RemoteAddr
+		ip := strings.Split(r.RemoteAddr, ":")[0]
 		apiKey := r.Header.Get("API_KEY")
 
-		rateLimiterQty, err := strconv.Atoi(ENVS.RateLimiterQtyIp)
+		rateLimiterToken, err := strconv.Atoi(ENVS.RateLimiterQtyToken)
+		if err != nil {
+			fmt.Println("Erro na conversão:", err)
+			return
+		}
+		rateLimiterIp, err := strconv.Atoi(ENVS.RateLimiterQtyToken)
 		if err != nil {
 			fmt.Println("Erro na conversão:", err)
 			return
 		}
 
-		if limiterData[apiKey] > rateLimiterQty {
+		if limiterData[apiKey].Count >= rateLimiterToken {
 			w.WriteHeader(http.StatusTooManyRequests)
 			w.Write([]byte("Too many requests"))
 			return
 		}
 
-		limiterData[apiKey]++
+		if limiterData[ip].Count >= rateLimiterIp {
+			w.WriteHeader(http.StatusTooManyRequests)
+			w.Write([]byte("Too many requests"))
+			return
+		}
+
+		limiterInfo := limiterData[apiKey]
+		limiterInfo.Count++
+		limiterInfo.BlockTimestamp = time.Now()
+		limiterData[apiKey] = limiterInfo
 
 		fmt.Println("IP", ip)
-		fmt.Println("Contador2", limiterData[apiKey])
+		fmt.Println("Contador", limiterData[apiKey].Count)
+		fmt.Println("Timestamp", limiterData[apiKey].BlockTimestamp)
 		next.ServeHTTP(w, r)
 	})
 }
